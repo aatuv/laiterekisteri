0d4b224a43885678b6e7b4edfa05dfb466ba1450
Aatu Väisänen <44797079+aatuv@users.noreply.github.com>
2018-11-15 14:49:08 +0200

    aasdssad

--- loginController.js
+++ loginController.js
@@ -18,8 +18,19 @@ module.exports =
     
         connection.query('SELECT * FROM kayttaja WHERE TUNNUS = ? AND SALASANA = ?', [username, password], (error, results, fields) => {
             if (error) {
-                
+                return callback(new Error ('Virhe suorittaessa kyselyä'), null);
             }
+            if (rows.length !== 1) {
+                return callback(new Error ('Täsmälleen yhden käyttäjän löytäminen epäonnistui'), null);
+            }
+            if (rows[0].password === password) {
+                return callback(new Error ('Täsmälleen yhden käyttäjän löytäminen epäonnistui'), null);
+           }
+
+           if (rows[0].password === password) {
+            // you would probably want a more useful callback result than 
+            // just returning the username, but again - an example
+            return callback(null, rows[0].username);
         }
     }
 }
